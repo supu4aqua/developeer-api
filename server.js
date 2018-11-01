af296a796d81549f7133a8378bf2555d290ded64
fastlain <michaeldallain@gmail.com>
2018-11-01 15:07:52 -0700

    Add morgan logging and errorhandler

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const morgan = require('morgan');
+const errorhandler = require('errorhandler');
 
+// Configuration and Environment variables
 const { CLIENT_ORIGIN } = require('./config');
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-    origin: CLIENT_ORIGIN
-}));
+// Initiate app
+const app = express();
+
+// Middleware
+app.use(cors({ origin: CLIENT_ORIGIN }));
+app.use(morgan('dev'));
+app.use(errorhandler());
+
 
 app.get('/api/*', (req, res) => {
     res.json({ ok: true })
