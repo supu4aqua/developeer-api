48dc1b03694d66f15933185d54aa265350be9098
fastlain <michaeldallain@gmail.com>
2018-11-01 08:26:10 -0700

    Add CORS

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+const { CLIENT_ORIGIN } = require('./config');
 const PORT = process.env.PORT || 3000;
+
+app.use(cors({
+    origin: CLIENT_ORIGIN
+}));
 
 app.get('/api/*', (req, res) => {
     res.json({ ok: true })
