68214923513eab2d69146f8dc7ddad0f1bbe2337
fastlain <michaeldallain@gmail.com>
2018-11-08 15:16:53 -0800

    Change development PORT to not conflict with front-end server

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
     'mongodb://localhost/developeer';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
     'mongodb://localhost/test-developeer';
-exports.PORT = process.env.PORT || 3000;
+exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'testkey';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
